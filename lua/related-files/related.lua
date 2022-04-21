050e0cef3e74a5f6a32caac6ee347950af2c09b2
Eric Saxby <sax@livinginthepast.org>
2022-04-21 16:15:56 -0700

    Show only filename; selecting file navigates to file

--- lua/related-files/related.lua
+++ lua/related-files/related.lua
@@ -21,15 +21,17 @@ local related = function(opts)
       for name, path in string.gmatch(line, " @related %[(.+)%]%((.+)%)") do
         if name then
           local entry = {
-            bufnr = 1,
+            bufnr = opts.bufnr,
             col = 0,
             filename = path,
             finish = 0,
             lnum = 0,
             start = 0,
             text = name,
+            value = path,
           }
-          table.insert(related_files, entry)
+          -- table.insert(related_files, entry)
+          table.insert(related_files, entry.filename)
         end
       end
     end
@@ -49,9 +51,10 @@ M.current_file = function(opts)
     prompt_title = "Related Files",
     finder = finders.new_table {
       results = results,
-      entry_maker = make_entry.gen_from_quickfix(opts)
+      -- entry_maker = make_entry.gen_from_quickfix(opts)
+      entry_maker = make_entry.gen_from_file(opts)
     },
-    -- selection_strategy = "reset",
+    selection_strategy = "reset",
     sorter = conf.generic_sorter(opts),
     border = {},
     -- previewer = nil,
